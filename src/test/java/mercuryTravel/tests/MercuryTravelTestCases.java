82b4d105188392a5a83b1b98bb29d5b13e11cdd7
gnikolic <gnikolic2704@gmail.com>
2024-10-24 11:23:37 +0200

    first commit

--- src/test/java/mercuryTravel/tests/MercuryTravelTestCases.java
+++ src/test/java/mercuryTravel/tests/MercuryTravelTestCases.java
@@ -45,24 +45,4 @@ public class MercuryTravelTestCases extends BaseTest {
         homepage.LogOut();
 
     }
-
-//    @Test
-//    public void verifyUserRegisterWithCorrectData() throws Exception {
-//        extentTest = extent.createTest("TC-002 - Verify User Registration with correct credentials");
-//
-//        String firstName = configProperties.getProperty("firstName");
-//        String lastName = configProperties.getProperty("lastName");
-//        String emailAddress = configProperties.getProperty("userEmailAddress");
-//        String userPassword = configProperties.getProperty("registerUserPassword");
-//        String confirmPassword = configProperties.getProperty("confirmPassword");
-//        String countryCode = configProperties.getProperty("countryCode");
-//        String mobileNumber = configProperties.getProperty("mobileNumber");
-//
-//        homepage.registerUser(firstName,lastName, emailAddress, userPassword,confirmPassword,countryCode,mobileNumber);
-//
-//        String expectedRegistrationText = configProperties.getProperty("expectedSuccesfullRegistrationText");
-//        String actualRegistrationText = homepage.getSuccessfulRegistrationMessage();
-//        Assert.assertEquals(actualRegistrationText, expectedRegistrationText);
-//        extentTest.log(Status.INFO, "User Login Successful");
-//    }
 }
